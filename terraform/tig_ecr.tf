8ee124c1cba882f3cbfa32a3a1b6a44b77f5cffc
Simon Liu <simon.liu@jpl.nasa.gov>
2024-03-28 16:38:48 -0700

    /deploy sit

--- terraform/tig_ecr.tf
+++ terraform/tig_ecr.tf
@@ -12,30 +12,6 @@ resource aws_ecr_repository "lambda-image-repo" {
   tags = var.tags
 }
 
-/*
-resource null_resource ecr_login {
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-e", "-c"]
-    command = <<EOF
-      echo ${data.aws_ecr_authorization_token.token.password} | docker login -u AWS --password-stdin ${data.aws_ecr_authorization_token.token.proxy_endpoint}
-      EOF
-  }
-}
-
-
-resource null_resource upload_ecr_image {
-  depends_on = [null_resource.ecr_login]
-
-  provisioner "local-exec" {
-    interpreter = ["/bin/bash", "-e", "-c"]
-    command = <<EOF
-      docker pull --platform=linux/arm/v7 ${var.lambda_container_image_uri}
-      docker tag ${var.lambda_container_image_uri} ${aws_ecr_repository.lambda-image-repo.repository_url}:${local.ecr_image_tag}
-      docker push ${aws_ecr_repository.lambda-image-repo.repository_url}:${local.ecr_image_tag}
-      EOF
-  }
-}*/
-
 resource "null_resource" "upload_ecr_image" {
   depends_on = [random_integer.ecr_login_trigger]
 
@@ -54,6 +30,14 @@ resource "null_resource" "upload_ecr_image" {
       docker tag ${var.lambda_container_image_uri} ${aws_ecr_repository.lambda-image-repo.repository_url}:${local.ecr_image_tag}
       docker push ${aws_ecr_repository.lambda-image-repo.repository_url}:${local.ecr_image_tag}
     EOF
+
+    # Hide the output of the Docker login command
+    log = {
+      # Hide stdout
+      stdout = false
+      # Hide stderr
+      stderr = false
+    }
   }
 }
 

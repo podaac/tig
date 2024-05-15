883e0deb96e91998facbb509729dc645df0f017e
Simon Liu <simon.liu@jpl.nasa.gov>
2024-05-15 12:34:35 -0700

    /deploy sit

--- terraform_deploy/override.py
+++ terraform_deploy/override.py
@@ -3,6 +3,6 @@ import json
 
 if __name__ == '__main__':
 
-    data = {'module': [{'tig': {'source': sys.argv[1], 'lambda_container_image_uri': sys.argv[2]}}]}
+    data = {'module': {'tig': {'source': sys.argv[1], 'lambda_container_image_uri': sys.argv[2]}}}
     with open('override.tf.json', 'w') as f:
         json.dump(data, f)

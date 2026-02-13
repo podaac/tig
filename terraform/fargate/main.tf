b3cb8f7bc6727eb6a8970bf64e6416fa97342194
Simon Liu <simon.liu@jpl.nasa.gov>
2026-02-12 16:13:30 -0800

    remove terraform provider versions

--- terraform/fargate/main.tf
+++ terraform/fargate/main.tf
@@ -11,11 +11,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.31.0"
     }
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.2.0"
     }
     random = {
       source = "hashicorp/random"
